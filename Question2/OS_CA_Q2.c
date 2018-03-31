2f271c33a82d05da72fc071470f8209f17292d87
yeshwanthkonk <yeshwanthkonk@users.noreply.github.com>
2018-04-01 00:29:25 +0530

    First Attempt

---
+++ Question2/OS_CA_Q2.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+    int Proce;
+    printf("Enter Number of Processors to Be Submitted to the Processors : \n");
+    scanf("%d",&Proce);
+    int i,j,var;
+    char P[Proce][2];
+    int AT[Proce];
+    int BT[Proce],RT[Proce];
+    int WT[Proce];
+    int TRT[Proce];
+    int AvgWT,AvgTRT;
+    for(i=0;i<Proce;i++)
+    {
+        printf("Enter Arrival Time and Brust Time For the Process P%d : \n",i+1);
+        P[i][0] = 'P';
+        P[i][1] = i+1;
+        scanf("%d %d",&AT[i],&BT[i]);
+        //RT[i] = BT[i];
+    }
+    for(i=0;i<Proce;i++)
+    {
+        for(j=i+1;j<Proce;j++)
+        {
+            if(AT[i]>AT[j])
+            {
+                var = AT[i];
+                AT[i] = AT[j];
+                AT[j] = var;
+
+                var = BT[i];
+                BT[i] = BT[j];
+                BT[j] = var;
+
+                var = P[i][1];
+                P[i][1] = P[j][1];
+                P[j][1] = var;
+            }
+        }
+    }
+    for(i=0;i<Proce;i++)
+    {
+        printf("%c%d : %d \t %d ",P[i][0],P[i][1],AT[i],BT[i]);
+        printf("\n");
+    }
+}
